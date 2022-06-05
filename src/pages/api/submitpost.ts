5b912c5d554e4870832d78feb204f4e99ad57c75
unknown <khalilbohner@gmail.com>
2022-06-05 01:19:22 -0300

    :weary:

--- src/pages/api/submitpost.ts
+++ src/pages/api/submitpost.ts
@@ -7,77 +7,65 @@ import { fauna } from '../../services/faunadb';
 import { query as q } from 'faunadb';
 import { deleteFile } from '../../utils/deleteFile';
 
-// interface IRequest extends NextApiRequest {
-//     file: any;
-// }
-
-// const upload = multer({
-//     storage: multer.diskStorage({
-//         destination: join(process.cwd(), 'tmp'),
-//         filename: (req, file, cb) =>
-//             cb(null, new Date().getTime() + '-' + file.originalname)
-//     })
-// });
-
-// const handler = nc<NextApiRequest, NextApiResponse>({
-//     onError: (err, req, res, next) => {
-//         console.error(err.stack);
-//         res.status(500).end('Alguma coisa quebrou!');
-//     },
-//     onNoMatch: (req, res) => {
-//         res.status(404).end('Página não encontrada');
-//     }
-// });
-
-// handler.post('/api/submitpost', async (request: IRequest, response) => {
-//     const session = await getSession({ req: request });
-//     const { description } = request.body;
-//     const file = request.file;
-
-//     if (!session || !description) {
-//         deleteFile(join(process.cwd(), 'tmp', file.filename));
-//         return response.status(400).redirect('/');
-//     }
-
-//     if (!file) {
-//         return response.status(400).redirect('/');
-//     }
-
-//     const username = session.user.email.split('@')[0];
-
-//     try {
-//         const object = await fauna.query(
-//             q.Create(q.Collection('posts'), {
-//                 data: {
-//                     username,
-//                     main_picture: join(process.cwd(), 'tmp', file.filename),
-//                     description
-//                 }
-//             })
-//         );
-
-//         return response.status(201).json(object);
-//     } catch (err) {
-//         console.log(err);
-//         return response.status(500).json({ message: 'Perdoa nois, deu erro' });
-//     }
-// });
-
-// export const config = {
-//     api: {
-//         bodyParser: false
-//     }
-// };
-
-export default async function submitPost(
-    req: NextApiRequest,
-    res: NextApiResponse
-) {
-    if (req.method === 'POST') {
-        console.log(req.body);
-        res.status(200).send({ message: 'it worked (for no reason)' });
-    } else {
-        res.setHeader('Allow', 'POST');
-        return res.status(405).end('Method not allowed');
-    }
+interface IRequest extends NextApiRequest {
+    file: any;
 }
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: join(process.cwd(), 'tmp'),
+        filename: (req, file, cb) =>
+            cb(null, new Date().getTime() + '-' + file.originalname)
+    })
+});
+
+const handler = nc<NextApiRequest, NextApiResponse>({
+    onError: (err, req, res, next) => {
+        console.error(err.stack);
+        res.status(500).end('Alguma coisa quebrou!');
+    },
+    onNoMatch: (req, res) => {
+        res.status(404).end('Página não encontrada');
+    }
+});
+
+handler.post('/api/submitpost', async (request: IRequest, response) => {
+    console.log(request.body);
+    // const session = await getSession({ req: request });
+    // const { description } = request.body;
+    // const file = request.file;
+
+    // if (!session || !description) {
+    //     deleteFile(join(process.cwd(), 'tmp', file.filename));
+    //     return response.status(400).redirect('/');
+    // }
+
+    // if (!file) {
+    //     return response.status(400).redirect('/');
+    // }
+
+    // const username = session.user.email.split('@')[0];
+
+    // try {
+    //     const object = await fauna.query(
+    //         q.Create(q.Collection('posts'), {
+    //             data: {
+    //                 username,
+    //                 main_picture: join(process.cwd(), 'tmp', file.filename),
+    //                 description
+    //             }
+    //         })
+    //     );
+
+    //     return response.status(201).json(object);
+    // } catch (err) {
+    //     console.log(err);
+    //     return response.status(500).json({ message: 'Perdoa nois, deu erro' });
+    // }
+});
+
+export const config = {
+    api: {
+        bodyParser: false
+    }
+};
