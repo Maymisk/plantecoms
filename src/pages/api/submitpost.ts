ef706698bc133c7cc1757d97aab0139de203dedf
unknown <khalilbohner@gmail.com>
2022-06-05 01:27:09 -0300

    :weary:

--- src/pages/api/submitpost.ts
+++ src/pages/api/submitpost.ts
@@ -19,44 +19,43 @@ const upload = multer({
     })
 });
 
-const handler = nc<NextApiRequest, NextApiResponse>();
+export default nc<NextApiRequest, NextApiResponse>().post(
+    '/api/submitpost',
+    async (request: IRequest, response) => {
+        console.log(request.body);
+        // const session = await getSession({ req: request });
+        // const { description } = request.body;
+        // const file = request.file;
 
-handler.post('/api/submitpost', async (request: IRequest, response) => {
-    console.log(request.body);
-    // const session = await getSession({ req: request });
-    // const { description } = request.body;
-    // const file = request.file;
+        // if (!session || !description) {
+        //     deleteFile(join(process.cwd(), 'tmp', file.filename));
+        //     return response.status(400).redirect('/');
+        // }
 
-    // if (!session || !description) {
-    //     deleteFile(join(process.cwd(), 'tmp', file.filename));
-    //     return response.status(400).redirect('/');
-    // }
+        // if (!file) {
+        //     return response.status(400).redirect('/');
+        // }
 
-    // if (!file) {
-    //     return response.status(400).redirect('/');
-    // }
+        // const username = session.user.email.split('@')[0];
 
-    // const username = session.user.email.split('@')[0];
+        // try {
+        //     const object = await fauna.query(
+        //         q.Create(q.Collection('posts'), {
+        //             data: {
+        //                 username,
+        //                 main_picture: join(process.cwd(), 'tmp', file.filename),
+        //                 description
+        //             }
+        //         })
+        //     );
 
-    // try {
-    //     const object = await fauna.query(
-    //         q.Create(q.Collection('posts'), {
-    //             data: {
-    //                 username,
-    //                 main_picture: join(process.cwd(), 'tmp', file.filename),
-    //                 description
-    //             }
-    //         })
-    //     );
-
-    //     return response.status(201).json(object);
-    // } catch (err) {
-    //     console.log(err);
-    //     return response.status(500).json({ message: 'Perdoa nois, deu erro' });
-    // }
-});
-
-export default handler;
+        //     return response.status(201).json(object);
+        // } catch (err) {
+        //     console.log(err);
+        //     return response.status(500).json({ message: 'Perdoa nois, deu erro' });
+        // }
+    }
+);
 
 export const config = {
     api: {
