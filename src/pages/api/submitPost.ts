fd3672e867ddc6af68a68a68d413147d900faf38
unknown <khalilbohner@gmail.com>
2022-06-04 23:26:14 -0300

    :weary:

--- src/pages/api/submitPost.ts
+++ src/pages/api/submitPost.ts
@@ -20,47 +20,53 @@ const upload = multer({
 });
 
 const handler = nc<NextApiRequest, NextApiResponse>({
+    onError: (err, req, res, next) => {
+        console.error(err.stack);
+        res.status(500).end('Alguma coisa quebrou!');
+    },
     onNoMatch: (req, res) => {
         res.status(404).end('Página não encontrada');
     }
 })
     .use(upload.single('file'))
-    .post(async (request: IRequest, response) => {
-        const session = await getSession({ req: request });
-        console.log(session);
-        const { description } = request.body;
-        const file = request.file;
+    .post((request: IRequest, response) => {
+        console.log('indo pegar a session');
+        getSession({ req: request }).then(session => {
+            console.log(session);
+            const { description } = request.body;
+            const file = request.file;
 
-        if (!session || !description) {
-            deleteFile(join(process.cwd(), 'tmp', file.filename));
-            return response.status(400).redirect('/');
-        }
+            if (!session || !description) {
+                deleteFile(join(process.cwd(), 'tmp', file.filename));
+                return response.status(400).redirect('/');
+            }
 
-        if (!file) {
-            console.log('file if');
-            return response.status(400).redirect('/');
-        }
+            if (!file) {
+                console.log('file if');
+                return response.status(400).redirect('/');
+            }
 
-        const username = session.user.email.split('@')[0];
+            const username = session.user.email.split('@')[0];
 
-        try {
-            const object = await fauna.query(
-                q.Create(q.Collection('posts'), {
-                    data: {
-                        username,
-                        main_picture: file.filename,
-                        description
-                    }
-                })
-            );
-
-            return response.status(201).json(object);
-        } catch (err) {
-            console.log(err);
-            return response
-                .status(500)
-                .json({ message: 'Perdoa nois, deu erro' });
-        }
+            try {
+                fauna
+                    .query(
+                        q.Create(q.Collection('posts'), {
+                            data: {
+                                username,
+                                main_picture: file.filename,
+                                description
+                            }
+                        })
+                    )
+                    .then(object => response.status(201).json(object));
+            } catch (err) {
+                console.log(err);
+                return response
+                    .status(500)
+                    .json({ message: 'Perdoa nois, deu erro' });
+            }
+        });
     });
 
 export const config = {
